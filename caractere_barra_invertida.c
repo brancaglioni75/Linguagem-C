a287fe419a11e575150355165f79751e958f26c8
Wilson Brancaglioni <70102493+brancaglioni75@users.noreply.github.com>
2022-02-07 20:56:42 -0300

    Add files via upload

---
+++ caractere_barra_invertida.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+main()
+{
+	
+	printf("Qual é o valor?\n");
+	printf("\'Linha' 1\n");
+	printf("\n10%%\n");
+	printf("\a\n");
+	printf("\a\n");
+	printf("Linha1a\n");
+	printf("Linha2a\t");
+	printf("Linha2b\n");
+	system("PAUSE");
+	return 0;
+	}	
+
