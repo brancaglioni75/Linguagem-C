92579e186d623078beebc87c33dc91aa7c07f0d3
Wilson Brancaglioni <70102493+brancaglioni75@users.noreply.github.com>
2022-02-07 21:59:27 -0300

    Add files via upload

---
+++ calculos.c
@@ -0,0 +1,29 @@
+/* Bibliotecas*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*================================================== 	
+
+	Alguns cálculos simples em linguagem C
+	Autor: Wilson Brancaglioni
+	Data: Fevereiro de 2022
+
+====================================================*/
+
+
+main() /* Função principal*/
+{
+/* Imprime o resultado de 1 + 1: */
+
+	printf("Resultado: %d\n\n", 1 + 1); /* usa-se %d para representar números inteiros*/
+	printf("Resultado: %d\n\n", 1 * 5);
+	printf("Resultado: %d\n\n", 8 - 1);
+	printf("Resultado: %d\n\n", 10 + 4);
+	printf("Resultado: %d\n\n", 12 / 6);
+	printf("Resultado: %d\n\n", 2 * 11);
+	printf("Resultado: %d\n\n", 25+5*2/5);
+	printf("Resultado: %d\n\n", (25+5)* 2/5);
+	
+	system("PAUSE");
+	return 0;
+}
